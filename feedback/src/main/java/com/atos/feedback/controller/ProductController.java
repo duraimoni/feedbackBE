febcc20b4e4d07b17519bb75df61e0d19e2707d6
chinmaytarwate <chinmaytarwate@gmail.com>
2019-02-26 18:31:40 +0530

    product controller

--- feedback/src/main/java/com/atos/feedback/controller/ProductController.java
+++ feedback/src/main/java/com/atos/feedback/controller/ProductController.java
@@ -10,5 +10,36 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.atos.feedback.service.ProductService;
+import com.atos.feedback.vo.ProductVO;
+
 @RestController
-@RequestMapping("user")
+@RequestMapping("product")
+public class ProductController() {
+
+    @Autowired
+    ProductService productService;
+
+    /*@PostMapping("add")
+    public String addProduct() {
+        productService.addProduct();
+        return "";
+    }
+
+    @PostMapping("update")
+    public String updateProduct() {
+        productService.updateProduct();
+        return "";
+    }
+
+    @PostMapping("delete")
+    public String deleteProduct() {
+        productService.deleteProduct();
+        return "";
+    }
+
+    @GetMapping("all")
+	public List<ProductVO> findAll() {
+		return null;
+	}*/
+}
