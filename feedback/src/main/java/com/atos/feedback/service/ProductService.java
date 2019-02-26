59b9c82af6b77a44f33351da7620ea82d1a6dcb4
chinmaytarwate <chinmaytarwate@gmail.com>
2019-02-26 18:43:14 +0530

    product service

---
+++ feedback/src/main/java/com/atos/feedback/service/ProductService.java
@@ -0,0 +1,11 @@
+package com.atos.feedback.service;
+
+import com.atos.feedback.model.Product;
+
+public interface ProductService {
+	/*String addProduct(Product product);
+
+    String updateProduct(Product product);
+    
+    String deleteProduct(Product product);*/
+}
