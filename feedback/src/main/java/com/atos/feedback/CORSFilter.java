d72acfc4dbcc10091ac6723d41c7bb791e151c31
Rathakrishnan Duraimoni <rathakrishnan.duraimoni@atos.net>
2019-07-03 18:40:16 +0530

    proeprty configuration

--- feedback/src/main/java/com/atos/feedback/CORSFilter.java
+++ feedback/src/main/java/com/atos/feedback/CORSFilter.java
@@ -10,26 +10,33 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CORSFilter implements Filter {
 
+	@Autowired
+	private Environment env;
+
 	public CORSFilter() {
-	   
+
 	}
+
 	@Override
-	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
 
- 
-	    HttpServletResponse response = (HttpServletResponse) res;
-
-	    response.setHeader("Access-Control-Allow-Origin","https://post-adm-product-feedback.bps.fr.atos.net");
-	    response.setHeader("Access-Control-Allow-Credentials", "true");
-	    response.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,OPTIONS");
-	    response.setHeader("Access-Control-Max-Age", "3600");
-	    response.setHeader("Access-Control-Allow-Headers", "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");
-	    chain.doFilter(req, res);
+		HttpServletResponse response = (HttpServletResponse) res;
+	//	System.out.println("what is the value="+env.getProperty("frontendurl"));
+		response.setHeader("Access-Control-Allow-Origin", env.getProperty("frontendurl"));//"https://post-adm-product-feedback.bps.fr.atos.net");
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+		response.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,OPTIONS");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Headers",
+				"Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");
+		chain.doFilter(req, res);
 	}
 
 	@Override
---
+++ feedback/src/main/java/com/atos/feedback/config/PropertyConfiguration.java
@@ -0,0 +1,11 @@
+package com.atos.feedback.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("file:C:\\RK\\ratingapp\\config.properties")
+public class PropertyConfiguration {
+
+    
+}
